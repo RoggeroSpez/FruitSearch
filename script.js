eb12f3828baeba2a4c597c47ff7ae84558a58d5d
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-05-21 12:37:56 -0700

    Updates

--- script.js
+++ script.js
@@ -3,26 +3,73 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
+let selectedSuggestionIndex = -1;
+let totalSuggestions = 0;
+
 function search(str) {
 	let results = [];
-
-	// TODO
-
+if (str.length > 0){
+results = fruit.filter(item => item.toLowerCase().includes(str.toLowerCase()));
+}
 	return results;
 }
 
 function searchHandler(e) {
-	// TODO
+const inputVal = e.target.value;
+const results = search(inputVal);
+showSuggestions(results, inputVal);
 }
 
 function showSuggestions(results, inputVal) {
+const fruitList = results.map((list, index)=>{return "<li index = '${index}'>@{list}</li>";})
+.join ('');
+suggestions,innerHTML = "<ul>" + fruitList + "</ul>";
+suggestions.style.display = "block";
 
-	// TODO
+totalSuggestions = results.length;
 }
 
 function useSuggestion(e) {
-	// TODO
-}
+  if (e.target.matches('li')) {
+    const selectedValue = e.target.textContent;
 
-input.addEventListener('keyup', searchHandler);
+    input.value = selectedValue;
+    suggestions.innerHTML = '';
+    selectedSuggestionIndex = -1;
+    input.focus();
+  } else if (e.key === 'ArrowUp') {
+    e.preventDefault();
+    if (selectedSuggestionIndex > 0) {
+      selectedSuggestionIndex--;
+    } else {
+      selectedSuggestionIndex = totalSuggestions - 1;
+    }
+    highlightSuggestion();
+  } else if (e.key === 'ArrowDown') {
+    e.preventDefault();
+    if (selectedSuggestionIndex < totalSuggestions - 1) {
+      selectedSuggestionIndex++;
+    } else {
+      selectedSuggestionIndex = 0;
+    }
+    highlightSuggestion();
+  } else if (e.key === 'Enter') {
+    const selectedSuggestion = suggestions.querySelector(`li[index="${selectedSuggestionIndex}"]`);
+    if (selectedSuggestion) {
+      const selectedValue = selectedSuggestion.textContent;
+
+      input.value = selectedValue;
+      suggestions.innerHTML = '';
+      selectedSuggestionIndex = -1;
+      input.focus();
+    }
+  }
+}
+input.addEventListener('keyup', function(e) {
+	if (e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'Enter') {
+	  useSuggestion(e);
+	} else {
+	  searchHandler(e);
+	}
+  });
 suggestions.addEventListener('click', useSuggestion);
